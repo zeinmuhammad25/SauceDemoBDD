2947b5955a42ba42a898947d285326eb8293ae56
zeinmuhammad25 <55272694+zeinmuhammad25@users.noreply.github.com>
2023-07-04 20:50:37 +0700

    Update LoginSteps.java

--- src/test/java/step_definitions/LoginSteps.java
+++ src/test/java/step_definitions/LoginSteps.java
@@ -1,41 +1,37 @@
 package step_definitions;
 
-//import cucumber.api.java.en.Given;
-//import cucumber.api.java.en.Then;
-//import cucumber.api.java.en.When;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.logging.Log;
 import org.example.pageObject.LoginPage;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 public class LoginSteps {
 
-    private WebDriver webDriver;
-
-    public LoginSteps(){
-        super();
-        this.webDriver = Hooks.webDriver;
-    }
-
+    public WebDriver driver = Hooks.driver;
+    LoginPage loginPage = new LoginPage(driver);
     @When("User input {string} as userName and input {string} as password")
-    public void inputCredential(String userName, String password) throws InterruptedException {
-        LoginPage loginPage = new LoginPage(webDriver);
+    public void inputCredential(String userName, String password){
         loginPage.setUserName(userName);
         loginPage.setPassword(password);
         loginPage.clickLogin();
     }
 
-    @Given("User already on website sauce demo")
-    public void verifyLandingPage(){
-        LoginPage loginPage = new LoginPage(webDriver);
-        Assert.assertTrue(loginPage.verifyLandingPage());
+    @Given("User already on login page")
+    public void verifyLoginPage(){
+        Assert.assertTrue(loginPage.verifyLoginPage());
     }
 
-    @Then("User see error {string} on login page")
-    public void verifyErrorText(String errorText){
-        LoginPage loginPage = new LoginPage(webDriver);
-        Assert.assertEquals(errorText, loginPage.verifyErrorText());
+    @Then("User already on product page")
+    public void verifyDashboard(){
+        Assert.assertTrue(loginPage.verifyDshboard());
     }
+
+    @Then("User see {string} as error text on login page")
+    public  void verifyErrorText(String A){
+        Assert.assertEquals( A , loginPage.textError());
+    }
+
 }
