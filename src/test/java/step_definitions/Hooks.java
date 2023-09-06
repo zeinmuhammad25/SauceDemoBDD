145a5f55204a3cba3e4a913c1fb9d7e30efeb891
zeinmuhammad25 <55272694+zeinmuhammad25@users.noreply.github.com>
2023-09-06 21:59:26 +0700

    Update Hooks.java

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -10,26 +10,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.HashMap;
 
 public class Hooks {
-
     public static WebDriver driver;
 
     @Before
-    public void openBrowser() throws InterruptedException {
-        ChromeOptions co = new ChromeOptions();
+    public void openBrowser(){
+        //WebDriverManager.chromedriver().clearDriverCache().setup();
+        //fungsi untuk setup chrome agar bisa terbuka
         WebDriverManager.chromedriver().setup();
+        ChromeOptions co = new ChromeOptions();
         co.addArguments("--remote-allow-origins=*");
-
+        //inisiasi library selenium
         driver = new ChromeDriver(co);
-        String URL = "https://www.saucedemo.com/";
-        driver.get(URL);
-        driver.manage().window().maximize();
 
+        String appUrl = "https://www.saucedemo.com/";
+        driver.get(appUrl);//fungsi untuk ngebuka link html
+        driver.manage().window().maximize();//fungsi untuk memaximize browser
     }
 
     @After
-    public void closeBrowser() throws InterruptedException {
+    public void closeBrowser(){
         driver.quit();
     }
-
-
 }
